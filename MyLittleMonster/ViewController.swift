a251a45823cbeceffc13263edc1a8a09678e0bfb
Zenun-Vee <zenunvucetovic@gmail.com>
2016-03-01 04:34:01 -0500

    Gigapet Game

--- MyLittleMonster/ViewController.swift
+++ MyLittleMonster/ViewController.swift
@@ -9,16 +9,113 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: MonsterImg!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
+    @IBOutlet weak var penalty1Img: UIImageView!
+    @IBOutlet weak var penalty2Img: UIImageView!
+    @IBOutlet weak var penalty3Img: UIImageView!
+  
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTY = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
+    
+    var monsterHappy = false
+    var currentItem:UInt32 = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        
+        penalty1Img.alpha = DIM_ALPHA
+        penalty2Img.alpha = DIM_ALPHA
+        penalty3Img.alpha = DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+    
+        startTimer()
     }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        monsterHappy = true
+        startTimer()
+        
+        foodImg.alpha = DIM_ALPHA
+        foodImg.userInteractionEnabled = false
+        heartImg.alpha = DIM_ALPHA
+        heartImg.userInteractionEnabled = false
+    
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+        
+    }
+    
+    func changeGameState() {
+        
+        
+        if !monsterHappy {
+            penalties += 1
+            
+            if penalties == 1 {
+                penalty1Img.alpha = OPAQUE
+                penalty2Img.alpha = DIM_ALPHA
+                
+            } else if penalties == 2 {
+                penalty2Img.alpha = OPAQUE
+                penalty3Img.alpha = DIM_ALPHA
+            } else if penalties >= 3 {
+                penalty3Img.alpha = OPAQUE
+            } else {
+                penalty1Img.alpha = DIM_ALPHA
+                penalty2Img.alpha = DIM_ALPHA
+                penalty1Img.alpha = DIM_ALPHA
+            }
+            
+            if penalties >= MAX_PENALTY {
+                gameOver()
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        }
+        
+        var rand = arc4random_uniform(2)
+        
+        if rand == 0 {
+            foodImg.alpha = DIM_ALPHA
+            foodImg.userInteractionEnabled = false
+            heartImg.alpha = OPAQUE
+            heartImg.userInteractionEnabled = true
+        } else {
+            heartImg.alpha = DIM_ALPHA
+            heartImg.userInteractionEnabled = false
+            foodImg.alpha = OPAQUE
+            foodImg.userInteractionEnabled = true
+        }
+        
+        currentItem = rand
+        monsterHappy = false
     }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+    }
+    
+    
+
 
 
 }
